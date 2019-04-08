3150d4ede6d34afb41fe5c991ef5b7ac935042bc
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-04-08 11:12:19 +0200

    Add accounts page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
+
+Vue.use(BootstrapVue)
+Vue.use(Router)
+Vue.use(VueAxios, axios)
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Accounts.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="main">
+        <h1>Accounts</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Username</th>
+                    <th scope="col">Platform</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="account in accounts">
+                    <th scope="row">{{ account.name }}</th>
+                    <td>PC</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+    export default {
+        name: 'Account',
+        data () {
+            return {
+                accounts: []
+            }
+        },
+        mounted () {
+            axios  
+            .get('http://localhost:8080/JEA6KillerAppV2/rest/account/', {
+                })
+            .then(response => (this.accounts = response.data))
+        }
+    }
+</script>
