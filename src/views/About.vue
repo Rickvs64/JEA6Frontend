82f5cb1915c15643fe41835df14cda809a39f606
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-04-25 11:48:42 +0200

    Modify About page to display app info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <h2>Welcome to the JEA6 Killer App.</h2>
+    <p>This app serves as a fanmade API-based website that fetches data (match history, performance, etc.) of each individual player and character to display for all to see.</p>
+    <p>The app consists of a separate front-end and back-end. The back-end stores data and contains admin login.</p>
   </div>
 </template>
