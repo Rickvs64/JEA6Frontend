095fe65e482acedd16c21e10338f725490c5b380
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-05-09 15:18:32 +0200

    Add Match history to Account page

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -2,6 +2,25 @@
     <div class="main">
         <h1>{{ account.name }}</h1>
         <h2>Platform: {{ account.platform }}</h2>
+
+        <h2>Matches played:</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Match ID</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- eslint-disable-next-line -->
+                <tr v-for="match in matches">
+                    <router-link :to="{ name: 'match', params: { id: match.id } }">
+                        <th>ID {{ match.id }}</th>
+                    </router-link>
+                    <td>{{ match.actualDate }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -13,7 +32,8 @@ import axios from 'axios'
         data () {
             return {
                 id: 0,
-                account: null
+                account: null,
+                matches: []
             }
         },
         mounted () {
@@ -23,8 +43,14 @@ import axios from 'axios'
         },
         methods: {
             getAccount() {
-                 axios.get('http://localhost:8080/JEA6KillerAppV2/rest/account/' + this.id).then((response) => {
+                axios.get('http://localhost:8080/JEA6KillerAppV2/rest/account/' + this.id).then((response) => {
                     this.account = response.data.result;
+                    this.getPlayedMatches();
+                });
+            },
+            getPlayedMatches() {
+                axios.get('http://localhost:8080/JEA6KillerAppV2/rest/match/byaccount/' + this.id).then((response) => {
+                    this.matches = response.data;
                 });
             }
         }
